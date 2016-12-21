66c29b53516f6c9b016a582c14271b5fb72b55cc
andreahassler <andrea.hassler@students.fhnw.ch>
2016-12-21 18:02:39 +0100

    ah

---
+++ src/app/ApplicationContext.java
@@ -0,0 +1,32 @@
+package app;
+
+import logic.Plate;
+
+/**
+ * This class is the central container for the main data structures and other information that is
+ * used throughout the application. This way, only the application context needs to be passed
+ * around the various classes instead of many individual variables.
+ */
+public class ApplicationContext {
+    private String version;
+    private String name;
+    private Plate plate;
+
+
+    public ApplicationContext(String version, String name) {
+        this.version = version;
+        this.name = name;
+        plate = new Plate(3,3);
+        //imageModel = new SimpleImageModel(gridData);
+
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,20 +1,62 @@
 package app;
 
-import logic.Rod;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import logic.Plate;
+import present.MainPanel;
 
 /**************************************
  * Created by Andrea on 06.11.2016.
  **************************************/
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) {
+
         //Plate plate = new Plate(6,6);
         //plate.fillArray();
         //String s = plate.toString();
         //System.out.println(s);
-        Rod rod = new Rod(11);
-        rod.fillArray();
-        System.out.print(rod.toString());
-        rod.gaussSeidelAlgorithm(0);
+       // Rod rod = new Rod(11);
+        //rod.fillArray();
+        //System.out.print(rod.toString());
+        //rod.gaussSeidelAlgorithm(0);
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final String VERSION = "1.0";
+        final String NAME = "SliceView FX";
+        final int WINDOW_WIDTH = 1000;
+        final int WINDOW_HEIGHT = 800;
+
+
+        // Create the core data structures
+
+        ApplicationContext applicationContext = new ApplicationContext(VERSION, NAME);
+
+        // Create the main user interface panel and provide it with the application context
+
+        Pane mainPane = new MainPanel(applicationContext, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        // Create and show the window
+
+        Scene scene = new Scene(mainPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        primaryStage.setTitle(applicationContext.getName() + " - v" + applicationContext.getVersion());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Plate plate = new Plate(4,4);
+        plate.fillArray();
+        String s = plate.toString();
+        System.out.println(s);
+        plate.gaussSeidelAlgorithm(0);
+        String a = plate.toString();
+        System.out.println(a);
+
     }
-}
+    public static void main(String[] args) {
+        launch(args);
+    }
+    }
+
